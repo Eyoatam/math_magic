19da5a8637e44fa914f6f38c1b663a7bcef7f7cb
Eyoatam <eyoatamtamirat7@gmail.com>
2021-03-31 16:13:26 +0300

    added type definitions

---
+++ typedef/magic.d.ts
@@ -0,0 +1,66 @@
+export declare class Magic {
+  compute2Digit(
+    num: number,
+  ): {
+    computedValue: number;
+    num: number;
+  };
+  compute3Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    num: number;
+  };
+  compute4Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    num: number;
+  };
+  compute5Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    thirdValue: number;
+    num: number;
+  };
+  compute6Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    thirdValue: number;
+    num: number;
+  };
+  compute7Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    thirdValue: number;
+    fourthValue: number;
+    num: number;
+  };
+  compute8Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    thirdValue: number;
+    fourthValue: number;
+    num: number;
+  };
+  compute9Digit(
+    num: number,
+  ): {
+    firstValue: number;
+    secondValue: number;
+    thirdValue: number;
+    fourthValue: number;
+    fifthValue: number;
+    num: number;
+  };
+}
